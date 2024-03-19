0746c4e69619107612fb1ab1add788aa3d1bf580
Cry-StalL <940755868@qq.com>
2024-03-19 19:49:53 +0800

    change to ChatAnywhere API

--- test/openai/multi_turn_test.py
+++ test/openai/multi_turn_test.py
@@ -5,7 +5,8 @@ from openai import OpenAI
 from config import api_config
 
 api_conf = api_config.Api_config()
-client = OpenAI(api_key=api_conf.openai_key)
+client = OpenAI(api_key=api_conf.openai_key,
+                base_url="https://api.chatanywhere.tech/v1")
 
 # 持续多轮对话，直到输入exit退出
 user_input = ""
