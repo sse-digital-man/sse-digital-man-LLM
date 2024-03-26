e66076cbf1b7579e982f0e4520191d7b6bd3846f
Cry-StalL <940755868@qq.com>
2024-03-26 16:28:58 +0800

    解决embedder重复引入的问题

--- utils/reload_database.py
+++ utils/reload_database.py
@@ -3,10 +3,9 @@ import sys
 sys.path.append(".")
 
 from config import db_config
-
+from core.db_operate.DB_Operator import DbOperator
 from core.db_operate.connection_handler import open_connection
 from core.db_operate.connection_handler import close_connection
-from core.db_operate.create_collection import create_collection
 from core.db_operate.create_embeddings import create_embeddings
 from utils.drop_database import drop_database
 
@@ -14,12 +13,13 @@ from utils.drop_database import drop_database
 # 重新执行create_collection和create_embeddings
 
 def reload_database():
+    db_operator = DbOperator()
     # if collection with the same name already exists, drop the original collection
     drop_database()
     # create collection
-    create_collection(db_config.COLLECTION_NAME)
+    db_operator.create_collection(db_config.COLLECTION_NAME)
     # create embeddings
-    create_embeddings(db_config.COLLECTION_NAME)
+    db_operator.create_embeddings(db_config.COLLECTION_NAME)
 
 open_connection()
 reload_database()
