1b540c4b50f92c4b719f89aa784975dde5833e0c
Cry-StalL <940755868@qq.com>
2024-05-27 17:23:04 +0800

    add answer interval

--- config/ConfigLoader.py
+++ config/ConfigLoader.py
@@ -5,7 +5,7 @@ import yaml
 
 class ConfigLoader:
     def __init__(self):
-        with open('config.yml', 'r') as f:
+        with open('config.yml', 'r', encoding='utf-8') as f:
             try:
                 config = yaml.safe_load(f)
             except yaml.YAMLError as exc:
@@ -13,6 +13,7 @@ class ConfigLoader:
 
         self.openai_key = config['api_keys']['llm']['openai_key']
         self.tts_model = config['api_config']['tts']['tts_model']
+        self.answer_interval = config['ue_config']['answer_interval']
         self.db_client_port = config['chroma_db_config']['db_client_port']
         self.db_collection_name = config['chroma_db_config']['db_collection_name']
         self.db_dimension = config['chroma_db_config']['db_dimension']
